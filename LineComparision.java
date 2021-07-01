b645b861d2663514f36cfb01667420ebe179aea5
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-02 00:42:06 +0530

    Welcome message

---
+++ LineComparision.java
@@ -0,0 +1,9 @@
+package lineComparsion;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
